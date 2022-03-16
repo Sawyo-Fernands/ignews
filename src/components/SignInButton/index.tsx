5ef312f900cfdb50de9a126f348d5f048734cbc2
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-03-16 14:46:41 -0300

    Componente : SubscribeButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss'
 
 export function SignInButton(){
 
-    const isSession=false
+    const isSession=true
 
     return isSession ? (
         <button type="button" className={styles.button}>
