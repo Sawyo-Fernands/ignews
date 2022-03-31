005d5920209fac07ea94bea3dc86a8110e8815d1
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-03-31 14:43:15 -0300

    Página: Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 import {RichText} from 'prismic-dom'
+import Link from 'next/link'
 
 type Post={
     slug:string;
@@ -29,11 +30,13 @@ export default function Posts({posts}:PostsProps){
                     {
                         posts.map(post=>(
                         <>
-                         <a href=''>
-                        <time>{post.updatedAt}</time>
-                        <strong>{post.title}</strong>
-                        <p>{post.excerpt}</p>
-                         </a>
+                        <Link href={`/posts/${post.slug}`}>
+                            <a >
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                         </>
                         ))
                     }
